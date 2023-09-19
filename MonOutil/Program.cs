7f0b9b60e50374e566c2c983ae2e367ec361b208
eJwtjMtuwjAUBf/F6yxI4zjESwRCYtFFWwo7y49LMHFi6zoPKOLfa6HobI5GmnkSqTXE+ONb6AknlEKtWF2bShWVLiua5yVlkhaFXJuPNKoZg1qTjAT0kzWAi/pcSmJYUs3Vi/Putzme97cv9b2f2CZsD9ONhdXmgJ/d5VQ05fgXU+qtiOERIHkKJAImGrV/A2k62/MwKme1aOGRIUjDPTbpBJ+NEZBDJ63LZo/txfk5yXAPFiEKORDej869Xv9CRlAr <gabriel_cote1@hotmail.com>
2023-09-19 10:14:18 -0400

    Traduction Anglaise

--- MonOutil/Program.cs
+++ MonOutil/Program.cs
@@ -4,25 +4,25 @@
 
 using MonOutil;
 
-Console.WriteLine("Bonjour, quel est votre nom?");
+Console.WriteLine("Hello, what is your name?");
 
 // Code qui lit la console pour stocker le nom
 string username = Console.ReadLine();
 
 
 // Code qui accueille l'utilisateur par son nom
-Console.WriteLine($"Bienvenue {username} à MonOutil.");
+Console.WriteLine($"Welcome {username} to MonOutil.");
 
 new Meteo();
 
-double tempEnCelcius;
-double tempEnFahrenheit;
+double tempInCelcius;
+double tempInFahrenheit;
 string? line;
 
 
-Console.WriteLine("Bienvenue à l'outil Météo!");
-Console.WriteLine("Entrez la température (en Celsius) que vous souhaitez convertir:");
+Console.WriteLine("Wecome to Meteo tool!");
+Console.WriteLine("Enter the temperature (in Celsius) that you want to convert:");
 line = Console.ReadLine();
-tempEnCelcius = Convert.ToDouble(line);
-tempEnCelcius = (tempEnCelcius * 9 / 5) + 32;
-Console.WriteLine("La température en Fahrenheit: {tempEnFarenheit} ");
+tempInCelcius = Convert.ToDouble(line);
+tempInFahrenheit = (tempInCelcius * 9 / 5) + 32;
+Console.WriteLine($"The temperature in Fahrenheit is: {tempInFahrenheit} ");
