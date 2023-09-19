b207341eca92ebedfd2a2d4572121af1f0294955
eJwtjMtuwjAUBf/F6yxI4zjESwRCYtFFWwo7y49LMHFi6zoPKOLfa6HobI5GmnkSqTXE+ONb6AknlEKtWF2bShWVLiua5yVlkhaFXJuPNKoZg1qTjAT0kzWAi/pcSmJYUs3Vi/Putzme97cv9b2f2CZsD9ONhdXmgJ/d5VQ05fgXU+qtiOERIHkKJAImGrV/A2k62/MwKme1aOGRIUjDPTbpBJ+NEZBDJ63LZo/txfk5yXAPFiEKORDej869Xv9CRlAr <gabriel_cote1@hotmail.com>
2023-09-19 10:06:49 -0400

    Ajout Outil Meteo

--- MonOutil/Program.cs
+++ MonOutil/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+using MonOutil;
+
+Console.WriteLine("Bonjour, quel est votre nom?");
+
+// Code qui lit la console pour stocker le nom
+string username = Console.ReadLine();
+
+
+// Code qui accueille l'utilisateur par son nom
+Console.WriteLine($"Bienvenue {username} à MonOutil.");
+
+new Meteo();
+
+double tempEnCelcius;
+double tempEnFahrenheit;
+string? line;
+
+
+Console.WriteLine("Bienvenue à l'outil Météo!");
+Console.WriteLine("Entrez la température (en Celsius) que vous souhaitez convertir:");
+line = Console.ReadLine();
+tempEnCelcius = Convert.ToDouble(line);
+tempEnCelcius = (tempEnCelcius * 9 / 5) + 32;
+Console.WriteLine("La température en Fahrenheit: {tempEnFarenheit} ");
